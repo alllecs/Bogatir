22e87500110444d67c682f27d5193a32366db00c
Smirnov Alexander <alllecs@yandex.ru>
2014-11-17 21:33:35 +0400

    Добавлены правильные варианты, в заданиях, где нужно найти ошибку

---
+++ 1.3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+
+	i = 43;
+	printf("В году %d недель\n", i);
+}
---
+++ 1.5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int x;
+
+	if(x > 2)
+	x = 2;
+
+	if(x < 1)
+	x = 1;
+}

6798c3a150b85ab2f519db5ffe5845fd66ef6cd6
Smirnov Alexander <alllecs@yandex.ru>
2014-11-17 21:38:34 +0400

    Добавлен 1.6.с в котором объеденины 1.6.pritf.c и 1.6.puts.c

---
+++ 1.6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+        char string[] = "Владимир. Кремль. Москва.";
+        puts(string);
+}
+
+{
+        printf("Путин\n");
+        printf("Кремль\n");
+        printf("Москва\n");
+        return 0;
+}
+

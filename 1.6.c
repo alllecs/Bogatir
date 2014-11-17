aa6006a8777d1b5e5b02adadfdfeef2af9e99fb1
Smirnov Alexander <alllecs@yandex.ru>
2014-11-18 00:49:46 +0400

    Изменена программа 1.6.с

--- 1.6.c
+++ 1.6.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-int main()
+void main_puts(void)
 {
         char string[] = "Владимир. Кремль. Москва.";
         puts(string);
 }
 
+void main_printf(void)
 {
         printf("Путин\n");
         printf("Кремль\n");
@@ -13,3 +14,8 @@ int main()
         return 0;
 }
 
+int main(void)
+{
+main_puts();
+main_printf();
+}

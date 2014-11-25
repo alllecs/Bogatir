e3dbfd3e1796854891c115c1029bcc146507bb58
Smirnov Alexander <alllecs@yandex.ru>
2014-11-25 23:57:38 +0400

    Рабочая версия 1.10.с

--- 1.10.c
+++ 1.10.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 int print_str(int WIDTH, int x0, int w)
 {
+	int i;
+
+	for(i = 0; i < x0; i++) {
+                printf("-");
+        }
+        for(i = 0; i < x0 + w; i++) {
+                 printf("*");
+        }
+        for(i = 0; i < WIDTH; i++) {
+                printf("-");
+        }
+        return 0;
+
 }
 
 int main(void)
 {
-	int i;
-	print_str(WIDTH, x0, w);
-
-	for(i = 0; i < x0; i++) {
-		printf("-");
-	}
-	for(i = 0; i < x0 + w; i++) {
-		 printf("*");
-	}
-	for(i = 0; i < WIDTH; i++) {
-		printf("-");
-	}
+	print_str(3, 4, 5);
+	return 0;
 }

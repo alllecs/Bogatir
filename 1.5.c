dc0330c62e6e1d1497270ebddef163984e8fdbb7
Smirnov Alexander <alllecs@yandex.ru>
2014-11-26 13:02:33 +0400

    Изменен 1.5.с

--- 1.5.c
+++ 1.5.c
@@ -12,11 +12,14 @@ int main(void)
 
 	printf("x = ");
 	scanf("%d", x);
-	if(x > 2) {
+	if(x >= 2) {
 		printf("%d", x = 2);
 	}
-	if(x < 1) {
+	if(x <= 1) {
 		printf("%d", x = 1);
+	} else {
+	printf("%d\n", x);
 	}
+
 	return 0;
 }

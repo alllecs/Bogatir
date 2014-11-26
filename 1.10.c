658c2ff53817a37d53a203f4f0414ca5acf47fa7
Smirnov Alexander <alllecs@yandex.ru>
2014-11-26 12:22:00 +0400

    Исправлен вывод в задании 1.10

--- 1.10.c
+++ 1.10.c
@@ -8,18 +8,19 @@ int print_str(int WIDTH, int x0, int w)
 	for(i = 0; i < x0; i++) {
                 printf("-");
         }
-        for(i = 0; i < x0 + w; i++) {
+        for(i = 0; i < w; i++) {
                  printf("*");
         }
-        for(i = 0; i < WIDTH; i++) {
+        for(i = 0; i < WIDTH - x0 - w; i++) {
                 printf("-");
         }
+	printf("\n");
         return 0;
 
 }
 
 int main(void)
 {
-	print_str(3, 4, 5);
+	print_str(10, 3, 5);
 	return 0;
 }

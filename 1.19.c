5f2cbc79a1ed657350289e476e974096bd6f2575
Smirnov Alexander <alllecs@yandex.ru>
2014-12-02 22:55:47 +0400

    Добавлены функции is_lower_latin(char ) и rotate_forward(char ) в 1.19.с Добавлены is_higher_latin(char ) и rotate_forward(char ) в 1.20.с

--- 1.19.c
+++ 1.19.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 
+int is_lower_latin(char symbol)
+{
+	if ((symbol >= 'a') && (symbol <= 'z'))
+		return 2;
+	else
+		return 0;
+}
+
+char rotate_forward(char symbol)
+{
+	if (symbol == 'z')
+		return 'a';
+	else
+		return ++symbol;
+}
+
 int main()
 {
 	char in_char;
 
 
 	while ((in_char = getchar()) != EOF) {
-		if ((in_char >= 'a') && (in_char <= 'z')) {
-			if (in_char == 'z')
-				in_char = 'a';
-			else
-				++in_char;
-		} else {
+		if (is_lower_latin(in_char))
+			in_char = rotate_forward(in_char);
+		else {
 		in_char = '*';
 		}
 
 	printf("%c", in_char);
 	}
+	return 0;
 }
--- 1.20.c
+++ 1.20.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 
+int is_higher_latin(char symbol)
+{
+	if ((symbol >= 'A') && (symbol <= 'Z'))
+		return 2;
+	else
+		return 0;
+}
+
+char rotate_forward(char symbol)
+{
+	if (symbol == 'Z')
+		return 'A';
+	else
+		return ++symbol;
+}
+
 int main()
 {
 	char in_char;
 
 
 	while ((in_char = getchar()) != EOF) {
-		if ((in_char >= 'A') && (in_char <= 'Z')) {
-			if (in_char == 'Z')
-				in_char = 'A';
-			else
-				++in_char;
-		} else {
+		if (is_higher_latin(in_char))
+			in_char = rotate_forward(in_char);
+		else {
 		in_char = '*';
 		}
 
 	printf("%c", in_char);
 	}
+	return 0;
 }
 
